df8cc9b3eca6c30097039101c3fe2a33c75faa5c
upupnoah <upupqi.cs@gmail.com>
2023-09-04 20:59:18 +0800

    feat: practical use of Makefile with hierarchical structure and wildcard patterns
    This commit includes the following changes related to Makefile:
    - Implemented a Makefile with a hierarchical structure to organize targets and dependencies.
    - Utilized wildcard patterns to simplify rule definitions and handle multiple files or directories.
    - Added rules for building, compiling, and executing specific targets or groups of targets.

---
+++ makefile-demo/make-rules/common.mk
@@ -0,0 +1,1 @@
+BLOCKER_TOOLS ?= gsemver
---
+++ makefile-demo/make-rules/tools.mk
@@ -0,0 +1,13 @@
+TOOLS ?=$(BLOCKER_TOOLS) $(CRITICAL_TOOLS) $(TRIVIAL_TOOLS)
+
+.PHONY: tools.install
+tools.install: $(addprefix tools.install., $(TOOLS))
+
+.PHONY: tools.install.%
+tools.install.%:
+	@echo "===========> Installing $*"
+	@$(MAKE) install.$*
+
+.PHONY: install.gsemver
+install.gsemver:
+	@$(GO) install github.com/arnaud-deprez/gsemver@latest
